32ea2a351d2c417afbf3f50685bcbd41887c29ea
José Manuel Alvarez <97788933+profjosemanuel@users.noreply.github.com>
2025-12-09 16:07:55 +0100

    Create crearestructuraarchivos.ps1

---
+++ crearestructuraarchivos.ps1
@@ -0,0 +1,20 @@
+# Ruta base donde se creará la estructura
+#$base = "C:\estructura\vehiculos"
+$base = ".\"
+# Crear estructura de carpetas
+$folders = @(
+ "$base\autobus",
+ "$base\aviones",
+ "$base\barcos",
+ "$base\bicicletas\bh",
+ "$base\bicicletas\orbea",
+ "$base\coches"
+)
+
+foreach ($folder in $folders) {
+ New-Item -ItemType Directory -Force -Path $folder | Out-Null
+}
+
+# Mostrar estructura en forma de árbol
+Tree $base
+
